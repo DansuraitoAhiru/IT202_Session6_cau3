0b1b7017e607add082dfbe3700584d29aa90d6e0
DansuraitoAhiru <vqviet2007@gmail.com>
2026-04-21 16:40:51 +0700

    Update cau3.sql

--- cau3.sql
+++ cau3.sql
@@ -2,7 +2,7 @@
 -- Tạo 1 bảng bookings gồm booking_id tự tăng, status, user_id
 -- sau đó đặt điều kiện nếu status = 'CANCELLED' thì trả về 1, còn ko thì trả về 0
 -- rồi dùng hàm sum để tính xem tổng số lần trả về 1
--- cụ thể là mỗi lần status = 'CANCELLED' thì sẽ trả về 1 hoặc 0 nếu dùng count nó sẽ đếm cả những lần trả về 0
+-- cụ thể là mỗi lần status = 'CANCELLED' thì sẽ trả về 1 hoặc 0 nếu dùng count nếu dùng ko chuẩn có thể sẽ bị đếm cả những lần trả về 0
 -- vd như có 7 lần 1 và 3 lần 0, dùng count thì sẽ đếm ra 10, còn sum thì chỉ là 7 thôi
 -- tiếp là nhóm tổng số lần Hủy này theo user_id
 -- đặt điều kiện ở having, count(*)>10, số lần hủy>5
@@ -60,7 +60,7 @@ VALUES
 	('COMPLETED', 1);
     
 SELECT user_id, count(*) AS total_bookings,
-sum(CASE WHEN status = 'CANCELLED' THEN 1 ELSE 0 END) as total_cancelled
+sum(CASE WHEN status = 'CANCELLED' THEN 1 ELSE 0 END) as total_cancelled    -- dùng COUNT(CASE WHEN status = 'CANCELLED' THEN 1 END) cũng đc
 FROM bookings
 GROUP BY user_id
 HAVING total_bookings >10 AND total_cancelled >5; 
